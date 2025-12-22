8adb5feb6b531398dbaa52a774ac14ea72bf59bb
Amir31-scurt <msarre3124@gmail.com>
2025-12-22 20:18:58 +0000

    fixed: update at

---
+++ prisma/migrations/20251222201813_mod/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `pdatedAt` on the `Product` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "pdatedAt",
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "updatedAt" DROP DEFAULT;
